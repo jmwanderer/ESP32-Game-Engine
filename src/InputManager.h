d5fc474677abaa0d242a4910da7ac3b460f37d9d
jim <jmwanderer@gmail.com>
2025-02-20 14:30:35 -0800

    Use Button library

--- src/InputManager.h
+++ src/InputManager.h
@@ -7,8 +7,9 @@ class InputManager {
 public:
     void init();
     void update(unsigned long dt);
-    bool isButtonPressed();
-    bool isButtonDown();
+    bool isButtonPressed(int button_num=0);
+    bool isButtonDown(int button_num=0);
+    bool isButtonReleased(int button_num=0);
 
 private:
     bool buttonState;
